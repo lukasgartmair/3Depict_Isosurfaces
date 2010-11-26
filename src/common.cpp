8729a50ed82e577a2c0a03a86600389c7500d294
D Haley <mycae@yahoo.com>
2010-11-26 22:25:16 +0000

    * Update windows install 	- Better mouseover in plot 	- fix compile * Remove assert  blocking

--- src/common.cpp
+++ src/common.cpp
@@ -23,6 +23,6 @@ const char *DTD_NAME="threeDepict-state.dtd";
 //Program name
 const char *PROGRAM_NAME = "3Depict";
 //Program version
-const char *PROGRAM_VERSION = "0.0.2";
+const char *PROGRAM_VERSION = "0.0.3";
 //Path to font for Default FTGL  font
 const char *FONT_FILE= "FreeSans.ttf";
