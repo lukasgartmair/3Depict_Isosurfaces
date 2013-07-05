913c0f9d52781c2c38df737c4c634a226c8dda28
tehuser <tehuser@users.sourceforge.net>
2013-07-05 22:23:39 +0200

    * bump version number * Update TODO

--- src/common/basics.cpp
+++ src/common/basics.cpp
@@ -44,7 +44,7 @@ const char *DTD_NAME="threeDepict-state.dtd";
 //Program name
 const char *PROGRAM_NAME = "3Depict";
 //Program version
-const char *PROGRAM_VERSION = "0.0.13";
+const char *PROGRAM_VERSION = "0.0.14";
 //Path to font for Default FTGL  font
 const char *FONT_FILE= "FreeSans.ttf";
 
