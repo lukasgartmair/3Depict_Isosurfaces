88e9fafe969eeffdaf9196a6aa3b67825a27e1bb
tehuser <tehuser@users.sourceforge.net>
2013-11-21 20:47:53 +0100

    * Partial import of fix from internal repo for mac packaging

--- packaging/mac/3package.sh
+++ packaging/mac/3package.sh
@@ -11,10 +11,18 @@ make distclean > out.txt 2>&1
 VERSION=`cat ./configure.ac | grep '^\s*AC_INIT(' | awk -F, '{ print $2 } ' | sed 's/\s*\[//' | sed 's/\]\s*//' | sed 's/\ //g'`
 BUILT_PROGRAMS_DIR=.
 MAC_OS_VER=`sw_vers | grep ProductVersion | awk '{print $2}'`
+YEAR=`date | awk '{print $NF}'`
 
-#Set bundle version
-sed "s/BUNDLE_VERSION/${VERSION}/" ./3Depict.app/Contents/Info.plist > ./test.txt
-mv ./test.txt ./3Depict.app/Contents/Info.plist
+#Fix some semi-constant values
+#===
+INFO_PLIST="./3Depict.app/Contents/Info.plist"
+#Set program version
+sed -i "s/BUNDLE_VERSION/${VERSION}/" ${INFO_PLIST}
+#Set year
+sed -i "s/3Depict Copyright (C) 20[123][0-9]/${DATEYEAR}/" ${INFO_PLIST}
+#Set minimum OS numbber
+sed -i "s/MIN_OS_VER/${MAC_OS_VER}/"  ${INFO_PLIST}
+#===
 
 time ./makeMacOSXApp --update-config=yes --parallel=yes --debug=no > out.txt 2>&1
 if [ x"`tail -1 out.txt`" == x"Done" ] ; then
@@ -29,6 +37,12 @@ fi
 #Perform some QA checks
 #--
 #TODO: Check debug symbols are stripped
+
+#Ensure that program name not screwed up by XCode
+if [ x`grep ${INFO_PLIST} -Depict` != x"" ] ; then
+	echo "Program name incorrectly stored in package, is listed as _Depict, rather than $PROGRAM_NAME"
+	exit 1
+fi
 
 #--
 
