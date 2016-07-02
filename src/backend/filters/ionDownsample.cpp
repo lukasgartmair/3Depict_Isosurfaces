16a046e94c2aa58f1b99797acb2b5c5b5098f6b6
tehuser <tehuser@users.sourceforge.net>
2016-07-02 21:54:17 +0100

    * Fix crash bug when reading downsample state file

--- src/backend/filters/ionDownsample.cpp
+++ src/backend/filters/ionDownsample.cpp
@@ -88,7 +88,7 @@ void IonDownsampleFilter::initFilter(const std::vector<const FilterStreamData *>
 			rsdIncoming = new RangeStreamData;
 			*rsdIncoming=*c;
 
-			if(ionFractions.size() != c->rangeFile->getNumIons())
+			if(ionFractions.size() != c->rangeFile->getNumIons()+1)
 			{
 				//set up some defaults; seeded from normal
 				ionFractions.resize(c->rangeFile->getNumIons()+1,fraction);
@@ -464,7 +464,8 @@ void IonDownsampleFilter::getProperties(FilterPropGroup &propertyList) const
 
 	propertyList.setGroupTitle(curGroup,TRANS("Mode"));
 	curGroup++;
-	if(rsdIncoming && perSpecies)
+
+	if(rsdIncoming && perSpecies && rsdIncoming->enabledIons.size())
 	{
 		unsigned int typeVal;
 		if(fixedNumOut)
